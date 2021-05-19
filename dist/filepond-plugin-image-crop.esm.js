8af1574e000d38a8a43eab385f9dfc14ba7dd1a8
Rik Schennink <rikschennink@gmail.com>
2021-05-19 16:12:11 +0200

    fix types

--- dist/filepond-plugin-image-crop.esm.js
+++ dist/filepond-plugin-image-crop.esm.js
@@ -1,5 +1,5 @@
 /*!
- * FilePondPluginImageCrop 2.0.5
+ * FilePondPluginImageCrop 2.0.6
  * Licensed under MIT, https://opensource.org/licenses/MIT/
  * Please visit https://pqina.nl/filepond/ for details.
  */
